56032442291122e9d1f856f861c77402c2c87947
Alex Kryukov <alexx.kryukov@gmail.com>
2015-10-03 15:37:29 +0300

    test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,38 +5,38 @@ import javax.servlet.http.*;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.*;
 
-import com.google.gson.Gson;
 
 
 public class Main extends HttpServlet {
-  @Override
-  protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-      throws ServletException, IOException {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
 
-      showOverview(req,resp);
+        //showOverview(req, resp);
+        resp.getWriter().print(req.getReader().read());
 
-  }
+    }
 
-  @Override
-  protected void doPost(HttpServletRequest req, HttpServletResponse resp)
-          throws ServletException, IOException {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
 
-  }
+    }
 
-  private void showOverview(HttpServletRequest req, HttpServletResponse resp)
-      throws ServletException, IOException {
-    resp.setContentType("application/json");
-    resp.getWriter().print("Hello! It's a LinguaHack app");
-  }
+    /*private void showOverview(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        resp.setContentType("application/json");
+        resp.getWriter().print("Hello! It's a LinguaHack app");
+    }*/
 
 
-  public static void main(String[] args) throws Exception {
-    Server server = new Server(Integer.valueOf(System.getenv("PORT")));
-    ServletContextHandler context = new ServletContextHandler();
-    context.setContextPath("/");
-    server.setHandler(context);
-    context.addServlet(new ServletHolder(new Main()),"/*");
-    server.start();
-    server.join();
-  }
+    public static void main(String[] args) throws Exception {
+        Server server = new Server(Integer.valueOf(System.getenv("PORT")));
+        ServletContextHandler context = new ServletContextHandler();
+        context.setContextPath("/");
+        server.setHandler(context);
+        context.addServlet(new ServletHolder(new Main()), "/*");
+        server.start();
+        server.join();
+    }
 }
